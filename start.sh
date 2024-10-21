71e70ffa951d38a42bc78c275fa35845896b2daa
paerd666 <watrhedsmedg@proton.me>
2024-10-21 08:37:26 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=f2051c0d-9049-448d-96a5-349e6895c64a
 
 #请修改为自己设置的伪装站，不要带https://
 export ProxySite=www.holehike.com
